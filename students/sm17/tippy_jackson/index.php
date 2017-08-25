7ac8b2af383932bd5c5815671ee7d25d2f8b635c
l3acon <hello@kylebaconcreative.com>
2017-08-25 14:49:18 -0700

    Add Tippy content

--- students/sm17/tippy_jackson/index.php
+++ students/sm17/tippy_jackson/index.php
@@ -1,4 +1,3 @@
-<!-- NEED -->
 <div class="s2 c2 bioleft">
     <div class="bio_pic">
         <img src="students/sm17/tippy_jackson/portraits/tippy.jpg" alt="Tippy Jackson" />
@@ -13,10 +12,9 @@
     <p><strong>If you were to run into me at NY Fashion Week, I would most likely be</strong>: <br />I would sneak backstage and feel all of the fabrics I could get my hands on, in the least creepy way possible.</p>
     <p><strong>My favorite article of clothing is</strong>: <br />Anything that allows me to explore a full range of motion. Restrictive clothing will not be tolerated.</p>
     <p><strong>The song playing as I strut down the runway would be</strong>: <br />“Renegade” by Styx. Specifically, the guitar solo. Over and over and over again.</p>
-    <!-- <img class="artwork" src="students/sm17/tippy_jackson/" alt="img"> -->
-    <!-- <img class="artwork" src="students/sm17/tippy_jackson/" alt="img"> -->
-    <!-- <img class="artwork" src="students/sm17/tippy_jackson/" alt="img"> -->
-    <!-- <img class="artwork" src="students/sm17/tippy_jackson/" alt="img"> -->
+    <img class="artwork" src="students/sm17/tippy_jackson/image10.jpg" alt="img">
+    <img class="artwork" src="students/sm17/tippy_jackson/image57.jpg" alt="img">
+    <img class="artwork" src="students/sm17/tippy_jackson/image65.gif" alt="img">
 </div>
 <!--FOOTER -->
 <?php include 'footer.php'; ?>
